a6277e837cb24020776f057576427c6d96d8ec0b
Sergey-98 <yatsenko1998@yandex.ru>
2022-11-04 00:17:41 +0400

    feat: add setting sort value to url

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,9 +8,15 @@ async function App() {
   main.classList.add('main-container');
   main.append(renderSortingBlock());
   const sortList = document.querySelector('.sorting');
+  const submit = document.querySelector('.submit_button');
   let data = await getDataCards();
-  sortList.addEventListener('change', (event) => {
-    data = sorting(data, event.target.value);
+  submit.addEventListener('click', () => {
+    if ('URLSearchParams' in window) {
+      const url = new URL(window.location);
+      url.searchParams.set('sort', sortList.value);
+      window.history.pushState({}, '', url);
+    }
+    data = sorting(data, sortList.value);
     document.querySelector('.cards').remove();
     renderCards(data);
   });
